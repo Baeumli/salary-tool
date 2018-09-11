d84bee37fee2692c7f8f3ba7fa628155712fec6f
Baeumli <luca.baumgartner@stud.bbbaden.ch>
2018-09-11 08:29:36 +0200

    git commiiit

--- src/main/java/me/baeumli/idpa/lohnblatt/generator/Main_Controller.java
+++ src/main/java/me/baeumli/idpa/lohnblatt/generator/Main_Controller.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
 
@@ -17,6 +18,8 @@ public class Main_Controller implements Initializable {
     private TabPane tabPane;
     @FXML
     private WebView webView;
+    @FXML
+    private TextField txtfieldAHV;
     
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
@@ -25,5 +28,14 @@ public class Main_Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
+
+    @FXML
+    private void btnPersonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void txtfieldAHVOnAction(ActionEvent event) {
+    }
 }
